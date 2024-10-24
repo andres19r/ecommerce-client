7389e73d622730c48238600c101d6a4772a70260
Andres Rivero <andres19rivero@gmail.com>
2024-10-24 04:31:21 -0400

    feat: implement contact template

--- src/app/shopping/components/contact/contact.component.ts
+++ src/app/shopping/components/contact/contact.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
+  private readonly formBuilder = inject(FormBuilder);
+  contactForm: FormGroup = this.formBuilder.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    message: ['', [Validators.required]],
+  });
 
+  onSubmit(): void {
+    if (this.contactForm.invalid) return;
+
+    // send email
+    console.log(this.contactForm.value);
+  }
 }
