51e3683f897ac612c13c9fa4fb4a19d27e14fc73
Andres Rivero <andres19rivero@gmail.com>
2024-10-24 03:26:39 -0400

    feat: implement catalog component

--- src/app/shopping/components/catalog/catalog.component.ts
+++ src/app/shopping/components/catalog/catalog.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject, Signal } from '@angular/core';
+import { ProductCardComponent } from '../product-card/product-card.component';
+import { CategoryService } from '../../services/category.service';
+import { CommonModule } from '@angular/common';
+import { Category } from '../../interfaces/category.interface';
 
 @Component({
   selector: 'app-catalog',
   standalone: true,
-  imports: [],
+  imports: [ProductCardComponent, CommonModule],
   templateUrl: './catalog.component.html',
-  styleUrl: './catalog.component.scss'
+  styleUrl: './catalog.component.scss',
 })
 export class CatalogComponent {
+  private readonly categoryService = inject(CategoryService);
+  categories: Signal<Category[]> = this.categoryService.getAll()
 
 }
