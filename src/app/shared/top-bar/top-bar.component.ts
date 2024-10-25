68617d14a8095cd1ec17a8875710cd0813ef22f0
Andres Rivero <andres19rivero@gmail.com>
2024-10-25 06:55:52 -0400

    feat(home): add logout btn and adjust colors

--- src/app/shared/top-bar/top-bar.component.ts
+++ src/app/shared/top-bar/top-bar.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../auth/services/auth.service';
 
 @Component({
   selector: 'app-top-bar',
@@ -10,5 +11,11 @@ import { RouterModule } from '@angular/router';
   styleUrl: './top-bar.component.scss'
 })
 export class TopBarComponent {
+  private readonly authService = inject(AuthService)
+  private readonly router = inject(Router)
 
+  logout(): void {
+    this.router.navigateByUrl('/auth');
+    this.authService.logout();
+  }
 }
