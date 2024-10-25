aacd79669faeeebfad9c4ba68604de4374c2a54d
Andres Rivero <andres19rivero@gmail.com>
2024-10-25 16:41:23 -0400

    fix(catalog): add filter categories by the products that are not in the user cart

--- src/app/shopping/components/catalog/catalog.component.ts
+++ src/app/shopping/components/catalog/catalog.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject, Signal } from '@angular/core';
+import { Component, computed, inject, Signal } from '@angular/core';
 import { ProductCardComponent } from '../product-card/product-card.component';
 import { CategoryService } from '../../services/category.service';
 import { CommonModule } from '@angular/common';
 import { Category } from '../../interfaces/category.interface';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-catalog',
@@ -13,5 +14,18 @@ import { Category } from '../../interfaces/category.interface';
 })
 export class CatalogComponent {
   private readonly categoryService = inject(CategoryService);
+  private readonly cartService = inject(CartService);
   categories: Signal<Category[]> = this.categoryService.getAll();
+  userCart = this.cartService.getUserCart();
+
+  filteredCategories: Signal<Category[]> = computed(() => {
+    const categories = this.categories();
+    const cartItems = this.userCart().cartItems;
+    const cartProductIds = cartItems.map(item => item.product);
+
+    return categories.map(category => ({
+      ...category,
+      products: category.products.filter(product => !cartProductIds.includes(product.id))
+    }));
+  });
 }
