e8e7ccefc4bb2df59e0320bc267a0cd512df5661
Andres Rivero <andres19rivero@gmail.com>
2024-10-25 04:00:03 -0400

    fix(auth): bind register component with service and translate pages

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -4,7 +4,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { RegisterBody } from './interfaces/auth.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
@@ -16,18 +19,28 @@ import { RouterModule } from '@angular/router';
     MatButtonModule,
     ReactiveFormsModule,
     RouterModule,
+    CommonModule
   ],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
-  private formBuilder = inject(FormBuilder)
-  private emailregex = '[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}'
-  public regiterForm = this.formBuilder.group({
+  private readonly authService = inject(AuthService);
+  private readonly router = inject(Router);
+  private formBuilder = inject(FormBuilder);
+  private emailregex = '[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}';
+  public registerForm = this.formBuilder.group({
     firstName: ['', Validators.required],
     lastName: ['', Validators.required],
-    city: ['', Validators.required],
-    email: ['', [Validators.required, Validators.email, Validators.pattern(this.emailregex)]],
+    city: [''],
+    email: [
+      '',
+      [
+        Validators.required,
+        Validators.email,
+        Validators.pattern(this.emailregex),
+      ],
+    ],
     password: ['', Validators.required],
   });
   public hide = signal(true);
@@ -36,7 +49,24 @@ export class RegisterComponent {
     this.hide.set(!this.hide());
     event.stopPropagation();
   }
+
   onSubmit() {
-    console.log('onSubmit')
+    if (this.registerForm.invalid) return;
+
+    const body: RegisterBody = {
+      firstName: this.registerForm.get('firstName')!.value ?? '',
+      lastName: this.registerForm.get('lastName')!.value ?? '',
+      email: this.registerForm.get('email')!.value ?? '',
+      password: this.registerForm.get('password')!.value ?? '',
+      city: this.registerForm.get('city')!.value ?? '',
+    };
+
+    this.authService.register(body).subscribe((valid) => {
+      if (valid === true) {
+        this.router.navigateByUrl('/');
+      } else {
+        console.error(valid)
+      }
+    });
   }
 }
