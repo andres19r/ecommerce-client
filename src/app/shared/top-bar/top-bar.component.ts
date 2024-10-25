a423d8f8c9872c6383ec255ee567d1e185f3e1ac
Andres Rivero <andres19rivero@gmail.com>
2024-10-24 23:53:06 -0400

    fix(top-bar): fix display and add routerLinks

--- src/app/shared/top-bar/top-bar.component.ts
+++ src/app/shared/top-bar/top-bar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-top-bar',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, RouterModule],
   templateUrl: './top-bar.component.html',
   styleUrl: './top-bar.component.scss'
 })
